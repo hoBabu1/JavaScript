e0cd4714047f9cc880e667144582a39e8649a1f2
hoBabu <amansahkumar004@gmail.com>
2024-05-22 11:15:19 +0530

    use of let, var and const keyword

---
+++ letConstVar.js
@@ -0,0 +1,14 @@
+const name = "Aman kumar "
+let age = 55
+var gender = "Male"
+console.log(name);
+/**
+ * .table is use to print in table format 
+ *  */ 
+console.table([age , gender , name])
+
+/** const - cannot re assign value in const 
+ * var is having scope problem 
+ * let and const are used 
+ * Java Script i=engine is inside a browser 
+ */
