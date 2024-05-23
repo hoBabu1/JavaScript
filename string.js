24b69cb790467d8437e5a268b9965c571b6af7b6
hoBabu <amansahkumar004@gmail.com>
2024-05-23 19:49:43 +0530

    String Basics

---
+++ string.js
@@ -0,0 +1,12 @@
+/**
+ * String Interpolation
+ */
+
+let virtualName = "hoBabu"
+let name = "Aman"
+
+console.log(`My name is ${name} and my virtual name is ${virtualName}`);
+
+// declairing a string 
+let country = new String("INDIA")
+console.log(country.toLowerCase());
